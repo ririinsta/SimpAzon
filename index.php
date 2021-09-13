67fbcd249a0d385b6daea65df7843f241e5f3e1d
ririinsta <87190112+ririinsta@users.noreply.github.com>
2021-09-13 13:16:27 -0400

    Added index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>SimpAzon</title>
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <div class="header">
+            <a href="#" class="logo">SimpAzon</a>
+            <div>
+                <a class="active" href="#">Home</a>
+                <a href="#">My Orders</a>
+                <a href="#">About</a>
+            </div>
+        </div>
+    </body>
+</html>
