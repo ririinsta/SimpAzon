b677631dcca1a2ed247093f5922b7e8ab025c871
unknown <lungrbackup@protonmail.com>
2021-09-14 18:26:50 -0400

    finished

--- js/product.js
+++ js/product.js
@@ -1,11 +1,8 @@
 var items = [document.getElementById("imq1"), document.getElementById("imq2"), document.getElementById("imq3"), document.getElementById("imq4"), document.getElementById("imq5"), document.getElementById("imq6")]
 
 function openproduct(index){
-    if (document.cookie == "hardcoded"){
-
-    } else {
-        document.location = document.location.hostname + "/product.html?pid=" + index;
-    }
+    alert(getCookie("hardcoded"));
+    document.location = getCookie("hardcoded") + "?pid" + index;
 }
 function getUrlVars() {
     var vars = {};
--- js/setjuan.js
+++ js/setjuan.js
@@ -1,4 +1,21 @@
-document.onload = function () {
+var items;
+var itemsi;
+var itemsp;
+var itemsn;
+var itemis;
+var itemqs;
+var itemns;
+var itemps;
+
+window.onload = function () {
+    var items = [document.getElementById("imq1"), document.getElementById("imq2"), document.getElementById("imq3"), document.getElementById("imq4"), document.getElementById("imq5"), document.getElementById("imq6")];
+    var itemsi = [document.getElementById("imi1"), document.getElementById("imi2"), document.getElementById("imi3"), document.getElementById("imi4"), document.getElementById("imi5"), document.getElementById("imi6")];
+    var itemsp = [document.getElementById("imp1"), document.getElementById("imp2"), document.getElementById("imp3"), document.getElementById("imp4"), document.getElementById("imp5"), document.getElementById("imp6")]
+    var itemsn = [document.getElementById("imn1"), document.getElementById("imn2"), document.getElementById("imn3"), document.getElementById("imn4"), document.getElementById("imn5"), document.getElementById("imn6")]
+    var itemis = ["img/1.png", "", "", "", "", ""];
+    var itemqs = [10, 0, 0, 0, 0, 0];
+    var itemns = ["Wood (Long Distance Only)", "", "", "", "", ""];
+    var itemps = [3, 0, 0, 0, 0, 0];
     load();
 };
 
@@ -16,33 +33,40 @@ function load(){
     items.forEach(element => {
         if (itemqs[index] == 0){
             element.style = "display: none;";
+        } else {
+            element.textContent = "x" + itemqs[index];
         }
-        element.textContent = "x" + itemqs[index];
         index++;
     });
     var index = 0;
     itemsi.forEach(element => {
-        if (itemis[index] == 0){
+        if (itemis[index] == ""){
             element.style = "display: none;";
+        } else {
+            element.textContent = itemps[index];
         }
-        element.textContent = "x" + itemps[index];
         index++;
     });
     var index = 0;
     itemsp.forEach(element => {
         if (itemps[index] == 0){
             element.style = "display: none;";
+        } else {
+            element.textContent = "x" + itemps[index];
         }
-        element.textContent = "x" + itemps[index];
         index++;
     });
     var index = 0;
     itemsn.forEach(element => {
         if (itemns[index] == ""){
+            //alert(index++);
+            //document.getElementById(index++).style = "display: none;"
             element.style = "display: none;";
+            element.parentElement.parentElement.style= "display: none;"
+        } else {
+            element.src = itemns[index];
         }
-        element.src = itemns[index];
-        console.log(itemns[index])
+        //console.log(itemns[index])
         index++;
     });
 }
