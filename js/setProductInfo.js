2c9958c018c1ddaddffeb74a4f9f765742d4c1e9
unknown <lungrbackup@protonmail.com>
2021-09-15 16:25:30 -0400

    temp

--- js/setProductInfo.js
+++ js/setProductInfo.js
@@ -2,23 +2,21 @@ var itemis;
 var itemqs;
 var itemns;
 var itemps;
-var itemIndex = getUrlVars()[pid] - 1;
+var itemIndex = getUrlVars()["pid"] - 1;
 
-windows.onLoad() = function (){
+document.onload = setProduct();
+function setProduct(){
     itemis = ["img/1.png", "", "", "", "", ""];
     itemqs = [10, , 0, 0, 0, 0];
     itemns = ["Wood (Long Distance Only)", "", "", "", "", ""];
     itemps = [3, , 0, 0, 0, 0];
-    setProduct();
-}
-
-function setProduct(){
     var name = document.getElementById("productname");
     var image = document.getElementById("productpicture");
     var description = document.getElementById("discription");
     var price = document.getElementById("productprice");
 
+    document.getElementById("title").text = "SimpAzon | " + itemns[itemIndex];
     name.textContent = itemns[itemIndex];
     image.src = itemis[itemIndex];
-    price.textContent = itemps[itemIndex];
+    price.textContent = "C$" + itemps[itemIndex];
 }
