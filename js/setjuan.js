57a4e2c187fc8939d371cb90e45e13b88b8e8680
unknown <lungrbackup@protonmail.com>
2021-09-15 13:39:57 -0400

    fixed error
    if you made a change to the catolog in the code it wouldn't change

--- js/setjuan.js
+++ js/setjuan.js
@@ -12,14 +12,15 @@ window.onload = function () {
     itemsi = [document.getElementById("imi1"), document.getElementById("imi2"), document.getElementById("imi3"), document.getElementById("imi4"), document.getElementById("imi5"), document.getElementById("imi6")];
     itemsp = [document.getElementById("imp1"), document.getElementById("imp2"), document.getElementById("imp3"), document.getElementById("imp4"), document.getElementById("imp5"), document.getElementById("imp6")]
     itemsn = [document.getElementById("imn1"), document.getElementById("imn2"), document.getElementById("imn3"), document.getElementById("imn4"), document.getElementById("imn5"), document.getElementById("imn6")]
-    itemis = ["img/1.png", "img/1.png", "", "", "", ""];
-    itemqs = [10, 1, 0, 0, 0, 0];
-    itemns = ["Wood (Long Distance Only)", "Test Item", "", "", "", ""];
-    itemps = [3, 1, 0, 0, 0, 0];
+    itemis = ["img/1.png", "", "", "", "", ""];
+    itemqs = [10, , 0, 0, 0, 0];
+    itemns = ["Wood (Long Distance Only)", "", "", "", "", ""];
+    itemps = [3, , 0, 0, 0, 0];
     load();
 };
 
-function reload(reloaded){
+
+function load(){
     var index = 0;
     items.forEach(element => {
         if (itemqs[index] == 0){
@@ -58,49 +59,6 @@ function reload(reloaded){
         } else {
             element.textContent = itemns[index];
             console.log(element.textContent);
-        }
-        //console.log(itemns[index])
-        index++;
-    });
-}
-
-function load(){
-    var index = 0;
-    items.forEach(element => {
-        if (itemqs[index] == 0){
-            element.style = "display: none;";
-        } else {
-            element.textContent = "x" + itemqs[index];
-        }
-        index++;
-    });
-    var index = 0;
-    itemsi.forEach(element => {
-        if (itemis[index] == ""){
-            element.style = "display: none;";
-        } else {
-            element.textContent = itemps[index];
-        }
-        index++;
-    });
-    var index = 0;
-    itemsp.forEach(element => {
-        if (itemps[index] == 0){
-            element.style = "display: none;";
-        } else {
-            element.textContent = "C$" + itemps[index];
-        }
-        index++;
-    });
-    var index = 0;
-    itemsn.forEach(element => {
-        if (itemns[index] == ""){
-            //alert(index++);
-            //document.getElementById(index++).style = "display: none;"
-            element.style = "display: none;";
-            element.parentElement.parentElement.style= "display: none;"
-        } else {
-            element.src = itemns[index];
         }
         //console.log(itemns[index])
         index++;
