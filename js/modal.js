9d6ac9d38451a8935b74d99ab9da3648afe8683a
unknown <lungrbackup@protonmail.com>
2021-09-16 14:06:28 -0400

    study hall done

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,16 @@
 var pmodal;
 var lmodal;
+var dmodal;
 var span;
 var spanl;
 document.onload = open();
 function open(){
     span = document.getElementsByClassName("close")[0];
     spanl = document.getElementById("close");
+    spand = document.getElementById("dclose");
     pmodal = document.getElementById("purchasemodal");
     lmodal = document.getElementById("loginmodal");
+    dmodal = document.getElementById("debugmodal");
 }
 function pmodelOpen(){
     pmodal.style.display = "block";
@@ -15,10 +18,16 @@ function pmodelOpen(){
 function lmodelOpen(){
     lmodal.style.display = "block";
 }
+function dmodelopen(){
+    dmodal.style.display = "block";
+}
 span.onclick = function() {
     pmodal.style.display = "none";
 }
 spanl.onclick = function() {
+    lmodal.style.display = "none";
+}
+spand.onclick = function() {
     lmodal.style.display = "none";
 }
 window.onclick = function(event) {
--- js/setProductInfo.js
+++ js/setProductInfo.js
@@ -10,12 +10,14 @@ function setProduct(){
     itemqs = [10, , 0, 0, 0, 0];
     itemns = ["Wood (Long Distance Only)", "", "", "", "", ""];
     itemps = [3, 0, 0, 0, 0, 0];
-    itemst = [25, 0, 0, 0, 0, 0];
+    itemst = [10, 0, 0, 0, 0, 0];
     var name = document.getElementById("productname");
     var image = document.getElementById("productpicture");
     var description = document.getElementById("discription");
     var price = document.getElementById("productprice");
     var productstat = document.getElementById("productindacator");
+    var amount = document.getElementById("amount");
+    amount.value = itemst[getUrlVars()["pid"]-1];
 
     document.getElementById("title").text = "SimpAzon | " + itemns[itemIndex];
     name.textContent = itemns[itemIndex];
@@ -25,5 +27,14 @@ function setProduct(){
 
     } else {
         productstat.style.display = "none";
+    }
+}
+function setQuantity(){
+    var amount = document.getElementById("amount").value;
+    itemst[itemIndex] = amount;
+    if (itemst[itemIndex] <= 10){
+
+    } else {
+        document.getElementById("productindacator").style.display = "none";
     }
 }
