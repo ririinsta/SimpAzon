6e7feb5d44127d9b5add3059565e76db9a2c3085
ririinsta <lungrbackup@protonmail.com>
2021-09-15 21:43:50 -0400

    test

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,16 @@
 // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var modal;
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span;
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+document.onload = open();
+function open(){
+    span = document.getElementsByClassName("close")[0];
+    modal = document.getElementById("myModal");
+}
+function modelOpen(){
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
