df275f733e349afd12c7662459b41fec957f883e
unknown <lungrbackup@protonmail.com>
2021-09-22 13:36:13 -0400

    Update loginandshit.js

--- js/loginandshit.js
+++ js/loginandshit.js
@@ -1,11 +1,23 @@
-var cid = document.getElementById("citizenid");
-var pid = document.getElementById("usernumber");
+var cid = document.getElementById("citizenid"); //matches with cids
+var pid = document.getElementById("usernumber"); //match with cids
 
 function login(){
-    setCookie("userid", cid.value+"."+pid.value, 10)
-    var login = document.getElementById("loginmodal");
-    login.style.display = "none";
-    document.location = document.location;
+    if (getCookie("localtesting") == "true"){
+        var users = getCookie("accounts"); //Usernames
+        var cids = getCookie("accountCIDs"); //Citizen IDs
+        
+        if (cids[cid] != null){
+            alert("username: " + users[cid]);
+            setCookie("userid", cid.value+"."+pid.value, 10)
+            var login = document.getElementById("loginmodal");
+            login.style.display = "none";
+            document.location = document.location;
+        } else {
+            var login = document.getElementById("loginmodal");
+            login.style.display = "none";
+            document.location = document.location;
+        }
+    }
 }
 
 function checkLogin(){
