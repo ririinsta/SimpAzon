f7fad9ef2ee2707dfef0a1b30636761ba4fed14f
unknown <lungrbackup@protonmail.com>
2021-09-27 14:49:18 -0400

    lol
    had to hardcode setting the user info because it wouldnt work

--- js/modalsetting.js
+++ js/modalsetting.js
@@ -1,16 +1,15 @@
-document.onload = function () {
+window.onload = setAll();
+function setAll(){
+    onOpen();
+    setProduct();
+    setUserDetails();
+}
+function setUserDetails(){
+    console.log(getCookie("userid").split(".")[0] + " " + getCookie("userid").split(".")[1])
     var username = fakeuser[getCookie("userid").split(".")[1]];
-    if(getCookie("userid") == ""){
-        document.getElementById("orders").style.display = "none";
-        document.getElementById("username").style.display = "block";
-        document.getElementById("username").textContent = "Username: " + username;
-        document.getElementById("balance").textContent = "Balance: 1000";
-        document.getElementById("orderst").textContent = "Orders: " + fakeuser3[getCookie("userid").split(".")[1]][0];
-        if (document.location == document.location.host + "product.html"){
-            document.getElementById("carttext").textContent = '&#xE033;'
-            document.getElementById("buytext").textContent = '&#xE033;'
-        }
-    } else {
-        document.getElementById("orders").style.display = "";
-    }
+    //document.getElementById("orders").style.display = "none";
+    //document.getElementById("username").style.display = "block";
+    document.getElementById("username").textContent = "Username: " + username;
+    document.getElementById("balance").textContent = "Balance: 1000";
+    document.getElementById("orderamount").textContent = "Orders: " + fakeuser3[getCookie("userid").split(".")[1]][0];
 }
